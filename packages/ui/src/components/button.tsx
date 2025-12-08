8913257c758f7efa87531918967555404b640953
Th1nkK1D <witheep@gmail.com>
2025-12-08 21:32:45 +0700

    feat(ui): add button active state and compact size

--- packages/ui/src/components/button.tsx
+++ packages/ui/src/components/button.tsx
@@ -13,6 +13,12 @@ export class ElectionButton {
 	@Prop() disabled: boolean = false;
 
 	/**
+	 * Enable Compact size
+	 * @default false
+	 */
+	@Prop() compact = false;
+
+	/**
 	 * Additional overwriting tailwind classes
 	 */
 	@Prop() class: string;
@@ -23,7 +29,8 @@ export class ElectionButton {
 				<button
 					disabled={this.disabled}
 					class={twMerge(
-						'disabled:text-gray-2 not-disabled:hover:bg-purple-2 not-disabled:hover:border-purple-2 border-gray-2 flex flex-row items-center justify-center gap-2 rounded-full border-2 border-solid px-6 py-3 not-disabled:cursor-pointer',
+						'disabled:text-gray-2 not-disabled:hover:bg-purple-2 not-disabled:hover:border-purple-2 border-gray-2 not-disabled:hover:active:text-green-2 flex flex-row items-center justify-center gap-2 rounded-full border-2 border-solid transition-colors not-disabled:cursor-pointer not-disabled:hover:active:border-black not-disabled:hover:active:bg-black',
+						this.compact ? 'px-4 py-2' : 'px-6 py-3',
 						this.class,
 					)}
 				>
