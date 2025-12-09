12234ec5fe4416d6196f319961aaf12b568fea0a
Th1nkK1D <witheep@gmail.com>
2025-12-09 16:02:35 +0700

    feat(partymatch): set up nuxt project with tailwind and ui package

---
+++ apps/partymatch/nuxt.config.ts
@@ -0,0 +1,32 @@
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineNuxtConfig({
+	app: {
+		baseURL: '/partymatch/',
+		head: {
+			link: [
+				{
+					href: 'https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai+Looped:wght@400;600;700&family=Sriracha&display=swap',
+					rel: 'stylesheet',
+				},
+				{
+					href: 'https://design-systems.wevis.info/typography.css',
+					rel: 'stylesheet',
+				},
+			],
+		},
+	},
+	compatibilityDate: '2025-07-15',
+	css: ['./app/assets/css/main.css'],
+	devtools: { enabled: true },
+	vite: {
+		plugins: [tailwindcss()],
+		vue: {
+			template: {
+				compilerOptions: {
+					isCustomElement: (tag) => tag.startsWith('election-'),
+				},
+			},
+		},
+	},
+});
