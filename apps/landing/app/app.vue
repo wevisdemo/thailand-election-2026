60fb6cd88eedb92075588920ea0ba894b14d6be9
Th1nkK1D <witheep@gmail.com>
2025-12-09 15:51:04 +0700

    chore(landing): add about actions example

--- apps/landing/app/app.vue
+++ apps/landing/app/app.vue
@@ -3,13 +3,14 @@ import {
 	ElectionNavbar,
 	ElectionButton,
 	ElectionSharer,
+	ElectionAboutActions,
 	ElectionFooter,
 } from '@election/ui/vue';
 import projects from '@election/constants/projects.json';
 </script>
 
 <template>
-	<div class="flex flex-col">
+	<div class="flex flex-col gap-6">
 		<ElectionNavbar />
 		<h1 class="text-h1 font-sriracha text-center font-bold underline">
 			Landing!
@@ -20,6 +21,7 @@ import projects from '@election/constants/projects.json';
 		<ElectionButton class="typo-h9 font-kondolar self-center font-bold"
 			>เริ่มเลอ</ElectionButton
 		>
+		<ElectionAboutActions dataUrl="#" />
 		<ElectionSharer />
 		<ElectionFooter />
 	</div>
