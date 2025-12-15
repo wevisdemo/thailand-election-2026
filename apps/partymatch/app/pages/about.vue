cba3b5ea76daa4181261646af3a986b25d8c2de2
Monsicha <monsicha.srisuantang@gmail.com>
2025-12-14 19:59:54 -0500

    chore(partymatch): add about page

---
+++ apps/partymatch/app/pages/about.vue
@@ -0,0 +1,23 @@
+<script setup>
+import {
+	ElectionNavbar,
+	ElectionButton,
+	ElectionSharer,
+	ElectionAboutActions,
+	ElectionFooter,
+} from '@election/ui/vue';
+</script>
+
+<template>
+	<div class="flex flex-col gap-6">
+		<ElectionNavbar />
+		<h1 class="text-h1 font-sriracha text-center font-bold underline">About</h1>
+
+		<ElectionButton class="typo-h9 font-kondolar self-center font-bold"
+			>เริ่มเลอ</ElectionButton
+		>
+		<ElectionAboutActions dataUrl="#" />
+		<ElectionSharer />
+		<ElectionFooter />
+	</div>
+</template>
