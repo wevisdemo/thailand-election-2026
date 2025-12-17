5d336591a39feec0cbc0f9f6819c114a5be752eb
Th1nkK1D <witheep@gmail.com>
2025-12-17 16:38:59 +0700

    ci: setup build and deploy worflow

---
+++ bundle.sh
@@ -0,0 +1,9 @@
+# Copy apps build assets to the out folder
+mkdir -p out
+cp -r apps/landing/.output/public/* out
+
+mkdir -p out/partymatch
+cp -r apps/partymatch/.output/public/* out/partymatch
+
+mkdir -p out/politicalflashback
+cp -r apps/politicalflashback/out/* out/politicalflashback
