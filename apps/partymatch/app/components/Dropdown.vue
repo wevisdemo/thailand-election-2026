13b06c750932c5a742ba55863a584b7f60008963
Monsicha <monsicha.srisuantang@gmail.com>
2025-12-19 17:47:07 -0500

    fix(partymatch): fix dropdown options

--- apps/partymatch/app/components/Dropdown.vue
+++ apps/partymatch/app/components/Dropdown.vue
@@ -1,54 +1,96 @@
 <script setup>
-import { Column, asNumber, asString, Spreadsheet } from 'sheethuahua';
-
-const schema = Object({
-	id: Column('id', asNumber()),
-	name: Column('party_name69', asString()),
-	name66: Column('party_name66', asString().optional()),
-	logo: Column('logo', asString().optional()),
-});
-
-const options = ref([]);
 const selected = ref('');
+const options = ref([]);
+const isOpen = ref(false);
 
-async function fetchData() {
-	options.value = await Spreadsheet(
+const selectedOption = computed(() =>
+	options.value.find((opt) => opt.id === selected.value),
+);
+
+onMounted(async () => {
+	const { Column, asString, Spreadsheet, Object } = await import('sheethuahua');
+
+	const data = await Spreadsheet(
 		'1cg85RsWVrSTDgRsVMTsmbkABbDk8Y84kIU_SsRl_smQ',
-	).get('party', schema);
-}
+	).get(
+		'party',
+		Object({
+			id: Column('id', asString()),
+			name: Column('party_name69', asString()),
+			name66: Column('party_name66', asString().optional()),
+			logo: Column('logo', asString().optional()),
+		}),
+	);
 
-onMounted(() => {
-	fetchData();
+	options.value = (data || []).filter((party) => party.id);
+
+	document.addEventListener('click', (e) => {
+		if (!e.target.closest('.dropdown-container')) {
+			isOpen.value = false;
+		}
+	});
 });
 
-// const output = await Spreadsheet(
-// 	'1cg85RsWVrSTDgRsVMTsmbkABbDk8Y84kIU_SsRl_smQ',
-// ).get('party', schema);
-// console.log(output);
+const toggleDropdown = () => {
+	isOpen.value = !isOpen.value;
+};
+
+const selectOption = (option) => {
+	selected.value = option.id;
+	isOpen.value = false;
+};
 </script>
 
 <template>
-	<div
-		class="dropdown flex w-full cursor-pointer flex-row items-center justify-between bg-white"
-	>
-		<select
-			v-model="selected"
-			class="h-full w-full appearance-none border-none bg-transparent text-left outline-none"
+	<div class="dropdown-container relative w-full">
+		<div
+			class="flex h-13 w-full cursor-pointer flex-row items-center justify-between rounded-full border-2 border-black bg-white pr-4 pl-6"
+			@click="toggleDropdown"
 		>
-			<option disabled value="">เลือกพรรค</option>
-			<option v-for="option in options" :key="option.id" :value="option.id">
-				{{ option.name }}
-			</option>
-		</select>
-		<img src="/assets/images/icon-chevron-down.svg" />
+			<div class="flex w-full items-center gap-4">
+				<img
+					v-if="selectedOption?.logo"
+					:src="selectedOption.logo"
+					class="h-6 w-6 rounded-full object-contain"
+					alt=""
+				/>
+
+				<span>
+					{{
+						selectedOption
+							? `${selectedOption.name}${selectedOption.name66 ? ` / ${selectedOption.name66}` : ''}`
+							: 'เลือกพรรค'
+					}}
+				</span>
+			</div>
+			<img
+				src="/assets/images/icon-chevron-down.svg"
+				:class="{ 'rotate-180': isOpen }"
+				class="transition-transform"
+			/>
+		</div>
+
+		<div
+			v-if="isOpen"
+			class="absolute z-50 mt-2 max-h-[300px] w-full overflow-y-auto rounded-lg border-2 border-black bg-white shadow-lg"
+		>
+			<div
+				v-for="option in options"
+				:key="option.id"
+				class="hover:bg-gray-3 border-gray-3 flex cursor-pointer items-center gap-4 border-b px-4 py-2"
+				:class="{ 'bg-gray-2': selected === option.id }"
+				@click="selectOption(option)"
+			>
+				<img
+					v-if="option.logo"
+					:src="option.logo"
+					class="h-8 w-8 rounded-full object-contain"
+					alt=""
+				/>
+				<span>
+					{{ option.name }} {{ option.name66 ? `/ ${option.name66}` : '' }}
+				</span>
+			</div>
+		</div>
 	</div>
 </template>
-
-<style scoped>
-.dropdown {
-	height: 50px;
-	border: black 2px solid;
-	border-radius: 100px;
-	padding: 0 0.8rem 0 1.5rem;
-}
-</style>
