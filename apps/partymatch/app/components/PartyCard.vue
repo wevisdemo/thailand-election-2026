535bb54c8929e47b70daed221b7bd818b0863bc1
Monsicha <monsicha.srisuantang@gmail.com>
2025-12-19 21:43:27 -0500

    style(partymatch): adjust card layout

--- apps/partymatch/app/components/PartyCard.vue
+++ apps/partymatch/app/components/PartyCard.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { ElectionButton } from '@election/ui/vue';
+
 defineProps({
 	selectedParty: {
 		type: Object,
@@ -8,10 +10,23 @@ defineProps({
 </script>
 
 <template>
-	<div v-if="selectedParty">
-		<p>มาดูกันว่า {{ selectedParty.name }} ทำงานตรงใจคุณแค่ไหน</p>
-		<img src="/assets/images/hero-img.svg" />
-		<img :src="selectedParty.logo" alt="Party Logo" />
-		<button>เริ่มเล่น</button>
+	<div
+		v-if="selectedParty"
+		class="flex w-full flex-col items-center gap-6 rounded-2xl bg-white p-10 shadow-md"
+	>
+		<p>
+			มาดูกันว่า <b>พรรค{{ selectedParty.name }}</b> ทำงานตรงใจคุณแค่ไหน
+		</p>
+		<div class="relative">
+			<img src="/assets/images/neo-selected.svg" class="w-100" />
+			<img
+				:src="selectedParty.logo"
+				alt=""
+				class="absolute top-2/3 left-1/2 w-24 -translate-x-1/2 -translate-y-1/2 transform rounded-full bg-white p-1"
+			/>
+		</div>
+		<ElectionButton class="typo-h8 font-kondolar w-full font-bold">
+			เริ่มเล่น
+		</ElectionButton>
 	</div>
 </template>
