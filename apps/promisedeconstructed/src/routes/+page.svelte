ca47cae01f7ec38c2c4acf4badada48caa13d4c5
Th1nkK1D <witheep@gmail.com>
2025-12-23 18:56:24 +0700

    feat(promisedeconstructed): init sveltekit project with tailwind and shared packages

---
+++ apps/promisedeconstructed/src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import {
+		ElectionNavbar,
+		ElectionButton,
+		ElectionSharer,
+		ElectionAboutActions,
+		ElectionFooter,
+	} from '@election/ui/svelte';
+</script>
+
+<div class="flex flex-col gap-6">
+	<ElectionNavbar />
+	<h1 class="text-h1 font-sriracha text-center font-bold underline">
+		Promise Deconstructed
+	</h1>
+
+	<div class="self-center">
+		<ElectionButton twClass="typo-h9 font-kondolar font-bold">
+			เริ่มเลอ
+		</ElectionButton>
+	</div>
+	<ElectionAboutActions dataUrl="#" />
+	<ElectionSharer />
+	<ElectionFooter />
+</div>
