3aa9866e24c29b135bc452eb943afd8c33f6371e
keykunjaee <naanong.sa@gmail.com>
2025-12-24 23:11:30 +0700

    feat(landing): add favicon

--- apps/landing/app/app.vue
+++ apps/landing/app/app.vue
@@ -21,7 +21,10 @@ useHead({
 		{ charset: 'utf-8' },
 		{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
 	],
-	link: [{ rel: 'canonical', href: baseUrl }],
+	link: [
+		{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+		{ rel: 'canonical', href: baseUrl },
+	],
 });
 
 useSeoMeta({
