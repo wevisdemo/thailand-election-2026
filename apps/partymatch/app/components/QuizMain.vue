13c98cc3aa0e0d8dc9d402609bfa19989f73f330
Monsicha <monsicha.srisuantang@gmail.com>
2025-12-26 19:04:32 -0500

    feat(partymatch): add marked

--- apps/partymatch/app/components/QuizMain.vue
+++ apps/partymatch/app/components/QuizMain.vue
@@ -1,12 +1,15 @@
 <script setup>
-    const questions = ref([]);
-    const currentQuestionIndex = ref(0);
+import { marked } from 'marked';
+const questions = ref([]);
+const currentQuestionIndex = ref(0);
+const currentQuestion = computed(() => {
+	return questions.value[currentQuestionIndex.value] || {};
+});
+const renderMarkdown = (markdownText) => {
+	return marked.parse(markdownText || '');
+};
 
-    const currentQuestion = computed(() => {
-      return questions.value[currentQuestionIndex.value] || {};
-    });
-
-    onMounted(async () => {
+onMounted(async () => {
 	const { Column, asString, Spreadsheet, Object } = await import('sheethuahua');
 
 	const data = await Spreadsheet(
@@ -20,54 +23,69 @@
 			description: Column('desc', asString()),
 		}),
 	);
-    questions.value = data;
-    });    
+	questions.value = data;
+});
 </script>
 
 <template>
-    <div class="flex flex-col h-full relative">
-    <!-- Progress bar -->
-        <div class="section w-full py-8">
-            <div class="bg-green-2 w-full h-1 rounded"></div>
-        </div>
+	<div class="relative flex h-full flex-col">
+		<!-- Progress bar -->
+		<div class="section w-full py-8">
+			<div class="bg-green-2 h-1 w-full rounded"></div>
+		</div>
 
-        <!-- Question -->
-        <div class="section flex flex-row">
-            <img src="/assets/images/card-side.png" class="py-4 transform scale-x-[-1]"/>
-            <div class="text-center bg-white p-6 rounded-2xl shadow-md h-80 flex flex-col gap-4">
-                <h2 class="text-h8 font-kondolar font-bold">{{ currentQuestion.title }}</h2>
-                <p v-if="currentQuestion.title_full">ชื่อเต็ม: {{ currentQuestion.title_full }}</p>
-                <h3 class="font-bold" v-if="currentQuestion.description">รายละเอียด</h3>
-                <p v-if="currentQuestion.description">{{ currentQuestion.description }}</p>
-            </div>
-            <img src="/assets/images/card-side.png" class="py-4"/>
-        </div>
+		<!-- Question -->
+		<div class="section flex flex-row">
+			<img
+				src="/assets/images/card-side.png"
+				class="scale-x-[-1] transform py-4"
+			/>
+			<div
+				class="flex h-80 flex-col gap-4 rounded-2xl bg-white p-6 text-center shadow-md"
+			>
+				<h2 class="text-h8 font-kondolar font-bold">
+					{{ currentQuestion.title }}
+				</h2>
+				<p v-if="currentQuestion.title_full">
+					ชื่อเต็ม: {{ currentQuestion.title_full }}
+				</p>
+				<h3 class="font-bold" v-if="currentQuestion.description">รายละเอียด</h3>
+				<p
+					v-if="currentQuestion.description"
+					v-html="renderMarkdown(currentQuestion.description)"
+				></p>
+			</div>
+			<img src="/assets/images/card-side.png" class="py-4" />
+		</div>
 
-        <!-- Quiz Choices -->
+		<!-- Quiz Choices -->
+		<QuizChoices />
 
-        <!-- Quiz Navigation -->
-        
-            <button
-            v-if="currentQuestionIndex > 0"
-            class="p-6 font-kondolar cursor-pointer flex flex-row items-center gap-2 mb-0 mt-auto absolute bottom-0 left-0"
-            @click="currentQuestionIndex--">
-                <img src="/assets/images/arrow-left.svg" class="h-8" />
-                กลับ
-            </button>
-            <button
-            v-if="currentQuestionIndex < questions.length - 1"
-            class="p-6 font-kondolar cursor-pointer flex flex-row items-center gap-2 mb-0 mt-auto absolute bottom-0 right-0"
-            @click="currentQuestionIndex++">
-                ไปต่อ
-                <img src="/assets/images/arrow-right.svg" class="h-8"/>
-            </button>
-            <button
-            v-else-if="currentQuestionIndex === questions.length - 1"
-            class="p-6 font-kondolar font-bold cursor-pointer flex flex-row items-center gap-2 mb-0 mt-auto absolute bottom-0 right-0"
-            @click="currentQuestionIndex++"> 
-                ดูผลลัพธ์
-                <img src="/assets/images/icon-heart-ol.svg" class="h-8"/>
-            </button>
-        
-    </div>
+		<!-- Quiz Navigation -->
+
+		<button
+			v-if="currentQuestionIndex > 0"
+			class="font-kondolar absolute bottom-0 left-0 mt-auto mb-0 flex cursor-pointer flex-row items-center gap-2 p-6"
+			@click="currentQuestionIndex--"
+		>
+			<img src="/assets/images/arrow-left.svg" class="h-8" />
+			กลับ
+		</button>
+		<button
+			v-if="currentQuestionIndex < questions.length - 1"
+			class="font-kondolar absolute right-0 bottom-0 mt-auto mb-0 flex cursor-pointer flex-row items-center gap-2 p-6"
+			@click="currentQuestionIndex++"
+		>
+			ไปต่อ
+			<img src="/assets/images/arrow-right.svg" class="h-8" />
+		</button>
+		<button
+			v-else-if="currentQuestionIndex === questions.length - 1"
+			class="font-kondolar absolute right-0 bottom-0 mt-auto mb-0 flex cursor-pointer flex-row items-center gap-2 p-6 font-bold"
+			@click="currentQuestionIndex++"
+		>
+			ดูผลลัพธ์
+			<img src="/assets/images/icon-heart-ol.svg" class="h-8" />
+		</button>
+	</div>
 </template>
