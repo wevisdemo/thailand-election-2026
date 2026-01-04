e6707bd1b285076e4a6998034b55bca296ecad85
Monsicha <monsicha.srisuantang@gmail.com>
2026-01-04 15:11:05 -0500

    style(partymatch): update icon choices

--- apps/partymatch/app/components/QuizChoice.vue
+++ apps/partymatch/app/components/QuizChoice.vue
@@ -1,3 +1,59 @@
+<script setup>
+import InfoPopup from './InfoPopup.vue';
+import { onClickOutside } from '@vueuse/core';
+
+const props = defineProps({
+	buttonClass: {
+		type: String,
+		default: '',
+	},
+	buttonStyle: {
+		type: Object,
+		default: () => ({}),
+	},
+	icon: {
+		type: [Object, Function, String],
+		required: true,
+	},
+	logoSrc: {
+		type: String,
+		required: false,
+	},
+	label: {
+		type: String,
+		required: true,
+	},
+	showInfoIcon: {
+		type: Boolean,
+		default: false,
+	},
+	isMatch: {
+		type: Boolean,
+		default: false,
+	},
+	disabled: {
+		type: Boolean,
+		default: false,
+	},
+	selected: {
+		type: Boolean,
+		default: false,
+	},
+	isUnselected: {
+		type: Boolean,
+		default: false,
+	},
+	showPartyLogo: {
+		type: Boolean,
+		default: false,
+	},
+});
+
+const isInfoPopupVisible = ref(false);
+const popupContainer = ref(null);
+onClickOutside(popupContainer, () => (isInfoPopupVisible.value = false));
+</script>
+
 <template>
 	<div class="flex flex-col items-center gap-1 md:gap-2">
 		<div class="flex min-h-8 items-center">
@@ -5,34 +61,36 @@
 				v-if="selected"
 				src="/img/profile-pic.svg"
 				alt="Your choice"
-				class="h-8 w-8"
+				class="h-6 w-6 md:h-8 md:w-8"
 			/>
 			<img
 				v-if="showPartyLogo"
 				:src="logoSrc"
 				alt="Party's choice"
-				class="h-8 w-8 rounded-full object-contain"
+				class="h-6 w-6 rounded-full object-contain md:h-8 md:w-8"
 			/>
 		</div>
 		<button
 			:class="[
-				isMatch ? 'border-4 border-black' : '',
-				selected ? 'border-4 border-black' : '',
+				'border-bg flex items-center justify-center self-center rounded-full border-4 transition-all duration-200',
+				isMatch || selected ? 'border-4 border-black' : '',
 				isUnselected && !showPartyLogo
 					? 'border-gray-3 bg-bg border-4 opacity-75'
 					: '',
-				!disabled ? 'cursor-pointer hover:border-3 hover:border-black' : '',
+				!disabled ? 'cursor-pointer border-4 hover:border-black' : '',
 				buttonClass,
 			]"
 			:style="buttonStyle"
-			class="h-20 w-20 items-center justify-center self-center rounded-full"
+			class="h-20 w-20"
 			@click="$emit('click')"
 			:disabled="disabled"
 		>
 			<div
-				v-html="computedIconSvg"
-				class="h-20 w-20 items-center justify-center"
-			></div>
+				class="flex h-20 w-20 items-center justify-center transition-colors"
+				:class="[isUnselected && !showPartyLogo ? 'text-gray-3' : 'text-white']"
+			>
+				<slot name="icon"></slot>
+			</div>
 		</button>
 		<span class="mt-2">{{ label }}</span>
 		<img
@@ -41,7 +99,7 @@
 			class="z-1 h-6 cursor-pointer"
 			@click.stop="isInfoPopupVisible = true"
 		/>
-		<div ref="popupContainer">
+		<Teleport to="body">
 			<InfoPopup
 				v-if="isInfoPopupVisible"
 				:title="label"
@@ -49,75 +107,6 @@
 				content="งดออกเสียง = สส. เข้าประชุมแต่ไม่ออกเสียงว่าเห็นด้วยหรือไม่เห็นด้วยกับมติ อาจจะเพราะยังไม่ตัดสินใจหรือเลี่ยงความขัดแย้ง<br/><br/> ในกรณีที่ต้อง ใช้เสียงข้างมากในการชี้ขาด<span class='text-purple-1 font-bold'>การงดออกเสียงจะมีผลเท่ากับเป็นการไม่เห็นด้วยได้</span> เช่น ญัตติอภิปรายไม่ไว้วางใจรัฐมนตรีที่ต้องใช้คะแนนเสียงมากกว่ากึ่งหนึ่งของจำนวน สส. ที่มีอยู่ในสภา การงดออกเสียงจึงทำให้เกิดผลไม่เห็นด้วยกับมติ"
 				@close="isInfoPopupVisible = false"
 			/>
-		</div>
+		</Teleport>
 	</div>
 </template>
-
-<script>
-import InfoPopup from './InfoPopup.vue';
-export default {
-	components: { InfoPopup },
-	props: {
-		buttonClass: {
-			type: String,
-			default: '',
-		},
-		buttonStyle: {
-			type: Object,
-			default: () => ({}),
-		},
-		iconSvg: {
-			type: String,
-			required: true,
-		},
-		logoSrc: {
-			type: String,
-			required: false,
-		},
-		label: {
-			type: String,
-			required: true,
-		},
-		showInfoIcon: {
-			type: Boolean,
-			default: false,
-		},
-		isMatch: {
-			type: Boolean,
-			default: false,
-		},
-		disabled: {
-			type: Boolean,
-			default: false,
-		},
-		selected: {
-			type: Boolean,
-			default: false,
-		},
-		isUnselected: {
-			type: Boolean,
-			default: false,
-		},
-		showPartyLogo: {
-			type: Boolean,
-			default: false,
-		},
-	},
-	computed: {
-		computedIconSvg() {
-			if (this.isUnselected && !this.showPartyLogo) {
-				return this.iconSvg.replace(
-					/<path([^>]*)fill=['\"](.*?)['\"]/,
-					"<path$1fill='#BFBFBF'",
-				);
-			}
-			return this.iconSvg;
-		},
-	},
-	data() {
-		return {
-			isInfoPopupVisible: false,
-		};
-	},
-};
-</script>
