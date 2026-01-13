2c33dae6259447a93f5eaaa4d312f54378b7ac9e
rootEnginear <rootenginear@users.noreply.github.com>
2026-01-13 23:47:12 +0700

    feat(promisedeconstructed): select default party from param

--- apps/promisedeconstructed/src/constants/party.ts
+++ apps/promisedeconstructed/src/constants/party.ts
@@ -1,1 +1,17 @@
 export const ALL_PARTY_VALUE = 'all';
+
+export const ENTH_PARTY_LOOKUP: Record<string, string | undefined> = {
+	people: 'ประชาชน',
+	pheuthai: 'เพื่อไทย',
+	democrat: 'ประชาธิปัตย์',
+	thaipeople: 'ปวงชนไทย',
+	prachachart: 'ประชาชาติ',
+	thaisangthai: 'ไทยสร้างไทย',
+	forthecountry: 'เพื่อชาติไทย',
+	economic: 'เศรษฐกิจ',
+	okardmai: 'โอกาสใหม่',
+	rakchart: 'รักชาติ',
+	bhumjaithai: 'ภูมิใจไทย',
+	unitednation: 'รวมไทยสร้างชาติ',
+	thaipakdee: 'ไทยภักดี',
+};
